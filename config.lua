fc2c723d344a445c62feb910f75a77bb3e1703af
ThangEthan <thangvu201098@gmail.com>
2021-10-11 23:21:27 +0300

    First

---
+++ config.lua
@@ -0,0 +1,288 @@
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+-- general
+lvim.format_on_save = false
+lvim.lint_on_save = true
+lvim.colorscheme = "onedarker"
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.insert_mode["<C-h>"] = "<Left>"
+lvim.keys.insert_mode["<C-j>"] = "<Down>"
+lvim.keys.insert_mode["<C-k>"] = "<Up>"
+lvim.keys.insert_mode["<C-l>"] = "<Right>"
+
+lvim.keys.normal_mode["Y"] = "y$"
+lvim.keys.normal_mode["n"] = "nzzzv"
+lvim.keys.normal_mode["N"] = "Nzzzv"
+lvim.keys.normal_mode["J"] = "mzJ`z"
+
+vim.cmd([[
+  set relativenumber
+  set timeoutlen=200
+]])
+
+-- require'lspconfig'.yamlls.setup{
+--   settings = {
+--     yaml = {
+--       trace = {
+--         server = "verbose"
+--       },
+--       schemas = {
+--         kubernetes = "/*.yaml"
+--       },
+--       schemaDownload = {  enable = true },
+--       validate = true,
+--     }
+--   },
+--   cmd = { "/Users/thangvu/.local/share/nvim/lspinstall/yaml/node_modules/.bin/yaml-language-server", "--stdio" }
+-- }
+-- unmap a default keymapping
+-- lvim.keys.normal_mode["<C-Up>"] = ""
+-- edit a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- lvim.builtin.telescope.on_config_done = function()
+--   local actions = require "telescope.actions"
+--   -- for input mode
+--   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   -- for normal mode
+--   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+-- end
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+-- }
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- generic LSP settings
+-- you can set a custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+-- you can overwrite the null_ls setup table (useful for setting the root_dir function)
+-- lvim.lsp.null_ls.setup = {
+--   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+-- }
+-- or if you need something more advanced
+-- lvim.lsp.null_ls.setup.root_dir = function(fname)
+--   if vim.bo.filetype == "javascript" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--       or require("lspconfig/util").path.dirname(fname)
+--   elseif vim.bo.filetype == "php" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--   else
+--     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--   end
+-- end
+
+-- set a formatter if you want to override the default lsp one (if it exists)
+-- lvim.lang.python.formatters = {
+--   {
+--     exe = "black",
+--     args = {}
+--   }
+-- }
+-- set an additional linter
+-- lvim.lang.python.linters = {
+--   {
+--     exe = "flake8",
+--     args = {}
+--   }
+-- }
+-- Additional Plugins
+lvim.plugins = {
+  {"tpope/vim-surround"},
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopLine<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
+  {
+    "Pocco81/AutoSave.nvim",
+    config = function()
+      require("autosave").setup()
+    end,
+  },
+  {"morhetz/gruvbox"},
+    -- {"folke/tokyonight.nvim"}, {
+    --     "ray-x/lsp_signature.nvim",
+    --     config = function() require"lsp_signature".on_attach() end,
+    --     event = "InsertEnter"
+    -- }
+}
+
+  -- {"easymotion/vim-easymotion"},
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+-- }
+-- nvim_lsp["yamlls"].setup {
+--   settings = {
+--     yaml = {
+--       schemas = { kubernetes = "/*.yaml" },
+--     },
+--   },
+--   on_attach = on_attach
+-- }
+lvim.lang.yaml.lsp.setup.settings = {
+  yaml = {
+    hover = true,
+    completion = true,
+    validate = true,
+    schemaStore = {
+      enable = true,
+      url = "https://www.schemastore.org/api/json/catalog.json",
+    },
+    schemas = {
+      Kubernetes = {
+        "daemon.yaml",
+        "manager.yaml",
+        "restapi.yaml",
+        "role.yaml",
+        "role_binding.yaml",
+        "*onfigma*.yml",
+        "*ngres*.yml",
+        "*ecre*.yml",
+        "*eployment*.yml",
+        "*ervic*.yml",
+        "*onfigma*.yaml",
+        "*ngres*.yaml",
+        "*ecre*.yaml",
+        "*eployment*.yaml",
+        "*ervic*.yaml",
+        "kubectl-edit*.yaml",
+      },
+      ["https://json.schemastore.org/ansible-role-2.9.json"] = {
+        "tasks/*.yml",
+        "tasks/*.yaml",
+      },
+      ["https://json.schemastore.org/ansible-inventory.json"] = {
+        "inventories/*.yml",
+        "*nventori*/*.yml",
+        "staging.yml",
+        "production.yml",
+      },
+      ["https://json.schemastore.org/ansible-playbook.json"] = {
+        "playbook.yml",
+        "playbook.yaml",
+        "main.yml",
+      },
+      ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+      ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+      ["http://json.schemastore.org/stylelintrc"] = ".stylelintrc.{yml,yaml}",
+      ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+      ["http://json.schemastore.org/helmfile"] = "helmfile.{yml,yaml}",
+      ["http://json.schemastore.org/gitlab-ci"] = "/*lab-ci.{yml,yaml}",
+    },
+  },
+}
+
+lvim.lang.yaml.lsp.setup.settings = {
+  yaml = {
+    hover = true,
+    completion = true,
+    validate = true,
+    schemaStore = {
+      enable = true,
+      url = "https://www.schemastore.org/api/json/catalog.json",
+    },
+    schemas = {
+      Kubernetes = {
+        "daemon.yaml",
+        "manager.yaml",
+        "restapi.yaml",
+        "role.yaml",
+        "role_binding.yaml",
+        "*onfigma*.yml",
+        "*ngres*.yml",
+        "*ecre*.yml",
+        "*eployment*.yml",
+        "*ervic*.yml",
+        "*onfigma*.yaml",
+        "*ngres*.yaml",
+        "*ecre*.yaml",
+        "*eployment*.yaml",
+        "*ervic*.yaml",
+        "kubectl-edit*.yaml",
+      },
+      ["https://json.schemastore.org/ansible-role-2.9.json"] = {
+        "tasks/*.yml",
+        "tasks/*.yaml",
+      },
+      ["https://json.schemastore.org/ansible-inventory.json"] = {
+        "inventories/*.yml",
+        "*nventori*/*.yml",
+        "staging.yml",
+        "production.yml",
+      },
+      ["https://json.schemastore.org/ansible-playbook.json"] = {
+        "playbook.yml",
+        "playbook.yaml",
+        "main.yml",
+      },
+      ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+      ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+      ["http://json.schemastore.org/stylelintrc"] = ".stylelintrc.{yml,yaml}",
+      ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+      ["http://json.schemastore.org/helmfile"] = "helmfile.{yml,yaml}",
+      ["http://json.schemastore.org/gitlab-ci"] = "/*lab-ci.{yml,yaml}",
+    },
+  },
+}
+
+lvim.builtin.lualine.on_config_done = function(lualine)
+  local config = lualine.get_config()
+
+  table.insert(config.sections.lualine_c, {
+    function()
+      local utils = require "core.lualine.utils"
+      local filename = vim.fn.expand "%"
+      local kube_env = os.getenv "KUBECONFIG"
+      local kube_filename = "kubectl-edit"
+      if (vim.bo.filetype == "yaml") and (string.sub(filename, 1, kube_filename:len()) == kube_filename) then
+        return string.format("⎈  (%s)", utils.env_cleanup(kube_env))
+      end
+      return ""
+    end,
+  })
+  lualine.setup(config)
+end
