93eddb8ccfef23eee691cfc291bc93fb59a011a6
ThangEthan <thangvu201098@gmail.com>
2021-10-13 22:25:16 +0300

    delete unused things

--- config.lua
+++ config.lua
@@ -138,12 +138,6 @@ lvim.plugins = {
       require("autosave").setup()
     end,
   },
-  {"morhetz/gruvbox"},
-    -- {"folke/tokyonight.nvim"}, {
-    --     "ray-x/lsp_signature.nvim",
-    --     config = function() require"lsp_signature".on_attach() end,
-    --     event = "InsertEnter"
-    -- }
 }
 
   -- {"easymotion/vim-easymotion"},
@@ -213,76 +207,3 @@ lvim.lang.yaml.lsp.setup.settings = {
     },
   },
 }
-
-lvim.lang.yaml.lsp.setup.settings = {
-  yaml = {
-    hover = true,
-    completion = true,
-    validate = true,
-    schemaStore = {
-      enable = true,
-      url = "https://www.schemastore.org/api/json/catalog.json",
-    },
-    schemas = {
-      Kubernetes = {
-        "daemon.yaml",
-        "manager.yaml",
-        "restapi.yaml",
-        "role.yaml",
-        "role_binding.yaml",
-        "*onfigma*.yml",
-        "*ngres*.yml",
-        "*ecre*.yml",
-        "*eployment*.yml",
-        "*ervic*.yml",
-        "*onfigma*.yaml",
-        "*ngres*.yaml",
-        "*ecre*.yaml",
-        "*eployment*.yaml",
-        "*ervic*.yaml",
-        "kubectl-edit*.yaml",
-      },
-      ["https://json.schemastore.org/ansible-role-2.9.json"] = {
-        "tasks/*.yml",
-        "tasks/*.yaml",
-      },
-      ["https://json.schemastore.org/ansible-inventory.json"] = {
-        "inventories/*.yml",
-        "*nventori*/*.yml",
-        "staging.yml",
-        "production.yml",
-      },
-      ["https://json.schemastore.org/ansible-playbook.json"] = {
-        "playbook.yml",
-        "playbook.yaml",
-        "main.yml",
-      },
-      ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-      ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-      ["http://json.schemastore.org/stylelintrc"] = ".stylelintrc.{yml,yaml}",
-      ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-      ["http://json.schemastore.org/helmfile"] = "helmfile.{yml,yaml}",
-      ["http://json.schemastore.org/gitlab-ci"] = "/*lab-ci.{yml,yaml}",
-    },
-  },
-}
-
-lvim.builtin.lualine.on_config_done = function(lualine)
-  local config = lualine.get_config()
-
-  table.insert(config.sections.lualine_c, {
-    function()
-      local utils = require "core.lualine.utils"
-      local filename = vim.fn.expand "%"
-      local kube_env = os.getenv "KUBECONFIG"
-      local kube_filename = "kubectl-edit"
-      if (vim.bo.filetype == "yaml") and (string.sub(filename, 1, kube_filename:len()) == kube_filename) then
-        return string.format("⎈  (%s)", utils.env_cleanup(kube_env))
-      end
-      return ""
-    end,
-  })
-  lualine.setup(config)
-end
