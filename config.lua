5650b14d64b2ad0edcc1321afe700580d2bdbfdb
Vu Viet Thang <thangvu201098@gmail.com>
2022-08-09 21:56:25 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,60 +1,67 @@
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
+lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.lint_on_save = true
 lvim.colorscheme = "onedarker"
+vim.opt.clipboard = "unnamedplus,unnamed"
+vim.opt.relativenumber = true
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.insert_mode["<C-h>"] = "<Left>"
-lvim.keys.insert_mode["<C-j>"] = "<Down>"
-lvim.keys.insert_mode["<C-k>"] = "<Up>"
-lvim.keys.insert_mode["<C-l>"] = "<Right>"
-
-lvim.keys.normal_mode["Y"] = "y$"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["n"] = "nzzzv"
 lvim.keys.normal_mode["N"] = "Nzzzv"
 lvim.keys.normal_mode["J"] = "mzJ`z"
+-- lvim.keys.normal_mode["y"] = "\"*y"
+-- lvim.keys.normal_mode["Y"] = "v$\"*y"
+-- lvim.keys.normal_mode["dd"] = "\"*dd"
+-- lvim.keys.normal_mode["D"] = "\"*D"
+-- lvim.keys.normal_mode["p"] = "\"*p"
 
-vim.cmd([[
-  set relativenumber
-  set timeoutlen=200
-]])
+lvim.keys.insert_mode["<c-h>"] = "<Left>"
+lvim.keys.insert_mode["<C-j>"] = "<Down>"
+lvim.keys.insert_mode["<C-k>"] = "<Up>"
+lvim.keys.insert_mode["<C-l>"] = "<Right>"
+lvim.keys.insert_mode["<A-a>"] = "<Esc>A"
+lvim.keys.insert_mode["<A-i>"] = "<Esc>I"
 
--- require'lspconfig'.yamlls.setup{
---   settings = {
---     yaml = {
---       trace = {
---         server = "verbose"
---       },
---       schemas = {
---         kubernetes = "/*.yaml"
---       },
---       schemaDownload = {  enable = true },
---       validate = true,
---     }
---   },
---   cmd = { "/Users/thangvu/.local/share/nvim/lspinstall/yaml/node_modules/.bin/yaml-language-server", "--stdio" }
--- }
+--lvim.keys.visual_mode["y"] = "\"*y"
+
+lvim.builtin.project.patterns = { ".git" }
+lvim.builtin.project.detection_methods = { "pattern" }
+
 -- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
+-- lvim.keys.normal_mode["<C-Up>"] = false
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
 --   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
 --   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -62,27 +69,59 @@ vim.cmd([[
 --   name = "+Trouble",
 --   r = { "<cmd>Trouble lsp_references<cr>", "References" },
 --   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },%
 --   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 -- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.automatic_servers_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
 --   local function buf_set_option(...)
 --     vim.api.nvim_buf_set_option(bufnr, ...)
@@ -90,47 +129,57 @@ lvim.builtin.treesitter.highlight.enabled = true
 --   --Enable completion triggered by <c-x><c-o>
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
 
--- set a formatter if you want to override the default lsp one (if it exists)
--- lvim.lang.python.formatters = {
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "black", filetypes = { "python" } },
+--   { command = "isort", filetypes = { "python" } },
 --   {
---     exe = "black",
---     args = {}
---   }
+--     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
 -- }
--- set an additional linter
--- lvim.lang.python.linters = {
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
 --   {
---     exe = "flake8",
---     args = {}
---   }
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
 -- }
--- Additional Plugins
+
+-- Additional Plugin
 lvim.plugins = {
-  {"tpope/vim-surround"},
   {
     "phaazon/hop.nvim",
     event = "BufRead",
     config = function()
       require("hop").setup()
-      vim.api.nvim_set_keymap("n", "s", ":HopLine<cr>", { silent = true })
-      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "s", ":HopWord<cr>", { silent = true })
     end,
+  },
+  {
+    "tpope/vim-surround",
+    keys = { "c", "d", "y" }
+  },
+  {
+    "tpope/vim-commentary"
   },
   {
     "Pocco81/AutoSave.nvim",
@@ -140,70 +189,7 @@ lvim.plugins = {
   },
 }
 
-  -- {"easymotion/vim-easymotion"},
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
--- nvim_lsp["yamlls"].setup {
---   settings = {
---     yaml = {
---       schemas = { kubernetes = "/*.yaml" },
---     },
---   },
---   on_attach = on_attach
--- }
-lvim.lang.yaml.lsp.setup.settings = {
-  yaml = {
-    hover = true,
-    completion = true,
-    validate = true,
-    schemaStore = {
-      enable = true,
-      url = "https://www.schemastore.org/api/json/catalog.json",
-    },
-    schemas = {
-      Kubernetes = {
-        "daemon.yaml",
-        "manager.yaml",
-        "restapi.yaml",
-        "role.yaml",
-        "role_binding.yaml",
-        "*onfigma*.yml",
-        "*ngres*.yml",
-        "*ecre*.yml",
-        "*eployment*.yml",
-        "*ervic*.yml",
-        "*onfigma*.yaml",
-        "*ngres*.yaml",
-        "*ecre*.yaml",
-        "*eployment*.yaml",
-        "*ervic*.yaml",
-        "kubectl-edit*.yaml",
-      },
-      ["https://json.schemastore.org/ansible-role-2.9.json"] = {
-        "tasks/*.yml",
-        "tasks/*.yaml",
-      },
-      ["https://json.schemastore.org/ansible-inventory.json"] = {
-        "inventories/*.yml",
-        "*nventori*/*.yml",
-        "staging.yml",
-        "production.yml",
-      },
-      ["https://json.schemastore.org/ansible-playbook.json"] = {
-        "playbook.yml",
-        "playbook.yaml",
-        "main.yml",
-      },
-      ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-      ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-      ["http://json.schemastore.org/stylelintrc"] = ".stylelintrc.{yml,yaml}",
-      ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-      ["http://json.schemastore.org/helmfile"] = "helmfile.{yml,yaml}",
-      ["http://json.schemastore.org/gitlab-ci"] = "/*lab-ci.{yml,yaml}",
-    },
-  },
-}
